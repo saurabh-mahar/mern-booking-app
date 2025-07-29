745780367f572125fc08b2785be2cf071213849e
saurabhMahar <saurabhmahar100@gmail.com>
2025-07-29 21:40:16 +0530

    added search feature and e2e tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -23,7 +24,7 @@ const App = () => {
         path="/search"
         element={
           <Layout>
-            <p>Search Paage</p>
+            <Search />
           </Layout>
         }
       />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider } from './contexts/AppContext.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import { AppContextProvider } from "./contexts/AppContext.tsx";
+import { SearchContextProvider } from "./contexts/SearchContext.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 0,
-    }
-  }
-})
+    },
+  },
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppContextProvider>
+          <SearchContextProvider>
+            <App />
+          </SearchContextProvider>
+        </AppContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
