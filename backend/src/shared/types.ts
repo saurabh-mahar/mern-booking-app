3431d02e166032b7fa0cddc8a345a47ddbd8d8e2
saurabhMahar <saurabhmahar100@gmail.com>
2025-07-30 22:12:18 +0530

    adding stripe & booking feature & e2e test

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -13,7 +13,21 @@ export type HotelType = {
   starRating: number;
   imageUrls: string[];
   lastUpdated: Date;
+  bookings: BookingType[];
 };
+
+export type BookingType = {
+  _id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: Date;
+  checkOut: Date;
+  totalCost: number;
+}
 
 export type HotelSearchResponse = {
   data: HotelType[];
@@ -22,4 +36,18 @@ export type HotelSearchResponse = {
     page: number;
     pages: number;
   };
+}
+
+export type UserType = {
+    _id: string,
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string;
+};
+
+export type PaymentIntentResponse = {
+  paymentIntentId: string;
+  clientSecret: string;
+  totalCost: number;
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
+import Booking from "./pages/Booking";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -56,6 +57,14 @@ const App = () => {
 
       {isLoggedIn && (
         <>
+          <Route
+            path="/hotel/:hotelId/booking"
+            element={
+              <Layout>
+                <Booking />
+              </Layout>
+            }
+          />
           <Route
             path="/add-hotel"
             element={
